d76e0fced1fcdd156cca3ee143d4193493eb7056
Migs <miguelemmanueldailisan@gmail.com>
2023-01-07 19:42:28 +0800

    cleaned up code

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,11 @@ class MyWidget extends StatelessWidget {
               children: [
                 Card(
                     elevation: 50,
-                    color:const Color.fromRGBO(35, 30, 57, 1),
+                    color: const Color.fromRGBO(35, 30, 57, 1),
                     child: Container(
                       width: MediaQuery.of(context).size.width * .5,
-                      height: 450,
-                      padding:
-                          const EdgeInsets.only(top: 20),
+                      height: 440,
+                      padding: const EdgeInsets.only(top: 20),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -36,214 +35,123 @@ class MyWidget extends StatelessWidget {
                           ),
                           const SizedBox(height: 15),
                           const Text("Miguel Dailisan",
-                              style:
-                                  TextStyle(fontSize: 20, color: const Color.fromRGBO(152,177,203,1))),
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color:
+                                      const Color.fromRGBO(152, 177, 203, 1))),
                           const Text("DAVAO CITY",
-                              style:
-                                  TextStyle(fontSize: 13, color: const Color.fromRGBO(152,177,203,1))),
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color:
+                                      const Color.fromRGBO(152, 177, 203, 1))),
                           const SizedBox(height: 15),
-                          const Text("FrontEnd Developer",
-                              style:
-                                  TextStyle(fontSize: 12, color: const Color.fromRGBO(152,177,203,1))),
+                          const Text("Front End Developer",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color:
+                                      const Color.fromRGBO(152, 177, 203, 1))),
                           const SizedBox(height: 20),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [TextButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0))),
-                                side: MaterialStateProperty.all(BorderSide(color: Color(0xFF03bfcb), width: 1.0, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                padding: MaterialStateProperty.all(
-                                    const EdgeInsets.symmetric(
-                                        horizontal: 18, vertical: 20)),
-                                backgroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                foregroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                overlayColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                textStyle: MaterialStateProperty.all(const TextStyle(
-                                    fontSize: 11, color: Colors.black))),
-                            child: const Text("Messege"),
+                            children: [
+                              button1("Message"),
+                              button1("Following")
+                            ],
                           ),
-                          TextButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0))),
-                                side: MaterialStateProperty.all(BorderSide(color: Color(0xFF03bfcb), width: 1.0, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                padding: MaterialStateProperty.all(
-                                    const EdgeInsets.symmetric(
-                                        horizontal: 18, vertical: 20)),
-                                backgroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                foregroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                overlayColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                textStyle: MaterialStateProperty.all(const TextStyle(
-                                    fontSize: 11, color: Colors.black))),
-                            child: const Text("Following"),
-                          )
-                          ]
-                          ,
-                          ),
-                          SizedBox(height:30),
+                          SizedBox(height: 30),
                           Container(
-                            height: 150,
-                            width: MediaQuery.of(context).size.width,
-                            color: Color.fromRGBO(31, 26, 54, 1),
-                            padding: EdgeInsets.only(top: 10, left: 20),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-
-                              children: [
-                                const Text("skills",
-                              style:
-                                  TextStyle(fontSize: 12, color: const Color.fromRGBO(152,177,203,1), fontWeight: FontWeight.bold)),
-                                  SizedBox(height: 10,),
+                              height: 150,
+                              width: MediaQuery.of(context).size.width,
+                              color: Color.fromRGBO(31, 26, 54, 1),
+                              padding: EdgeInsets.only(top: 10, left: 20),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text("skills",
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: const Color.fromRGBO(
+                                              152, 177, 203, 1),
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
                                   Wrap(
                                     direction: Axis.horizontal,
                                     children: [
-                                      TextButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0))),
-                                side: MaterialStateProperty.all(BorderSide(color: Color.fromRGBO(152,177,203,1), width: 0.5, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                padding: MaterialStateProperty.all(
-                                    const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 10)),
-                                backgroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                foregroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                overlayColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                textStyle: MaterialStateProperty.all(const TextStyle(
-                                    fontSize: 11, color: Colors.black))),
-                            child: const Text("UI/UX"),
-                            ),
-
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                              child: TextButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                  shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0))),
-                                  side: MaterialStateProperty.all(BorderSide(color: Color.fromRGBO(152,177,203,1), width: 0.5, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                  padding: MaterialStateProperty.all(
-                                      const EdgeInsets.symmetric(
-                                          horizontal: 10, vertical: 10)),
-                                  backgroundColor:
-                                      MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                  foregroundColor:
-                                      MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                  overlayColor:
-                                      MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                  textStyle: MaterialStateProperty.all(const TextStyle(
-                                      fontSize: 11, color: Colors.black))),
-                              child: const Text("Front End"),
-                              ),
-                            ),
-
-                            TextButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0))),
-                                side: MaterialStateProperty.all(BorderSide(color: Color.fromRGBO(152,177,203,1), width: 0.5, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                padding: MaterialStateProperty.all(
-                                    const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 10)),
-                                backgroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                foregroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                overlayColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                textStyle: MaterialStateProperty.all(const TextStyle(
-                                    fontSize: 11, color: Colors.black))),
-                            child: const Text("HTML"),
-                            ),
-
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10, right: 10),
-                              child: TextButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                  shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0))),
-                                  side: MaterialStateProperty.all(BorderSide(color: Color.fromRGBO(152,177,203,1), width: 0.5, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                  padding: MaterialStateProperty.all(
-                                      const EdgeInsets.symmetric(
-                                          horizontal: 10, vertical: 10)),
-                                  backgroundColor:
-                                      MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                  foregroundColor:
-                                      MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                  overlayColor:
-                                      MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                  textStyle: MaterialStateProperty.all(const TextStyle(
-                                      fontSize: 11, color: Colors.black))),
-                              child: const Text("CSS"),
-                              ),
-                            ),
-
-                            TextButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0))),
-                                side: MaterialStateProperty.all(BorderSide(color: Color.fromRGBO(152,177,203,1), width: 0.5, style: BorderStyle.solid, strokeAlign: StrokeAlign.inside)),            
-                                padding: MaterialStateProperty.all(
-                                    const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 10)),
-                                backgroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
-                                foregroundColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(152,177,203,1)),
-                                overlayColor:
-                                    MaterialStateProperty.all(const Color.fromRGBO(3,191,203,1)),
-                                textStyle: MaterialStateProperty.all(const TextStyle(
-                                    fontSize: 11, color: Colors.black))),
-                            child: const Text("Javascript"),
-                            )
-                            
+                                      button2("UX/UI"),
+                                      button2("Front End Development"),
+                                      button2("HTML"),
+                                      button2("CSS"),
+                                      button2("JavaScript"),
+                                      button2("React"),
+                                      button2("Node")
                                     ],
                                   )
-                                
-                              ],
-                                  
-                            )
-                            
-
-                          )
-                          
+                                ],
+                              ))
                         ],
                       ),
-                    ))
-                    ,
-                    
+                    )),
               ],
             )));
+  }
+
+  Padding button1(String a) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 5),
+      child: TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0))),
+            side: MaterialStateProperty.all(BorderSide(
+                color: Color(0xFF03bfcb),
+                width: 1.0,
+                style: BorderStyle.solid,
+                strokeAlign: StrokeAlign.inside)),
+            padding: MaterialStateProperty.all(
+                const EdgeInsets.symmetric(horizontal: 18, vertical: 20)),
+            backgroundColor:
+                MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
+            foregroundColor: MaterialStateProperty.all(
+                const Color.fromRGBO(152, 177, 203, 1)),
+            overlayColor:
+                MaterialStateProperty.all(const Color.fromRGBO(3, 191, 203, 1)),
+            textStyle: MaterialStateProperty.all(
+                const TextStyle(fontSize: 11, color: Colors.black))),
+        child: Text(a),
+      ),
+    );
+  }
+
+  Padding button2(String b) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 3, right: 3, bottom: 5),
+      child: TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0))),
+            side: MaterialStateProperty.all(BorderSide(
+                color: Color.fromRGBO(152, 177, 203, 1),
+                width: 0.5,
+                style: BorderStyle.solid,
+                strokeAlign: StrokeAlign.inside)),
+            padding: MaterialStateProperty.all(
+                const EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+            backgroundColor:
+                MaterialStateProperty.all(const Color.fromRGBO(35, 30, 57, 1)),
+            foregroundColor: MaterialStateProperty.all(
+                const Color.fromRGBO(152, 177, 203, 1)),
+            overlayColor:
+                MaterialStateProperty.all(const Color.fromRGBO(3, 191, 203, 1)),
+            textStyle: MaterialStateProperty.all(
+                const TextStyle(fontSize: 11, color: Colors.black))),
+        child: Text(b),
+      ),
+    );
   }
 }
